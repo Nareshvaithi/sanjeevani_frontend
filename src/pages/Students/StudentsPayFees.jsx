944c6dcada7a511ba04a375f39dad3a75ac04d18
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2025-03-14 19:39:11 +0530

    add payment

--- src/pages/Students/StudentsPayFees.jsx
+++ src/pages/Students/StudentsPayFees.jsx
@@ -1,40 +1,138 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { parse,format } from "date-fns";
+import { useOutletContext } from "react-router-dom";
 import { useRazorpay, RazorpayOrderOptions } from "react-razorpay";
-import { useOutletContext } from 'react-router-dom';
+import { addPayment } from "../../store/adminSlices/adminStudentsSlice";
+import { useDispatch } from "react-redux";
+import { fetchSingleStudent, setSingleStudentRecord } from "../../store/formSlices/StudentDetailsSlice";
 
 function StudentsPayFees() {
+  const dispatch=useDispatch()
   const API_URL = import.meta.env.VITE_API_URL;
-const studentDetails=useOutletContext()
-
-const [data, setData] = useState([]);
-console.log(studentDetails)
-  useEffect(()=>{
-    const payment=async()=>{
-      const response=await axios.get(`${API_URL}/payments/paymentsall`)
-    setData(response.data)
-    let id
-    if(studentDetails.age>18){
-      data.map((value)=>{
-        if(value.plan=="Adult")
-        id=value._id
+  const studentDetails = useOutletContext();
+  const { error, isLoading, Razorpay } = useRazorpay();
+  const [data, setData] = useState([]);
+  console.log(studentDetails);
+  useEffect(() => {
+    const payment = async () => {
+      const response = await axios.get(`${API_URL}/payments/paymentsall`);
+      setData(response.data);
+    };
+    payment();
+  }, []);
+console.log(data)
+const handlePayment=async()=>{
+  let id;
+  if (data.age > 18) {
+    data.map((value) => {
+      if (value.plan == "Adult") id = value._id;
+    });
+  } else {
+    data.map((value) => {
+      if (value.plan == "Child") id = value._id;
+    });
+  }
+  if (!id) throw new Error("Payment plan not found");
+  const paidPayment = await axios.get(`${API_URL}/payments?_id=${id}`);
+  console.log(paidPayment.data)
+  const orderDetails={received_payment:paidPayment.data.amount,paymentOderID:paidPayment.data.id,_id:studentDetails._id}
+  const handler = (response) => {
+    const order = {
+      ...orderDetails,
+      paymentId: response.razorpay_payment_id,
+    };
+    console.log(order)
+    dispatch(
+      addPayment({
+        ...orderDetails,
+        paymentId: response.razorpay_payment_id,
       })
-    }else{
-      data.map((value)=>{
-        if(value.plan=="Child")
-        id=value._id
-      })
+    );
+    // dispatch(fetchSingleStudent)
+    dispatch(fetchSingleStudent({ ...orderDetails,
+      paymentId: response.razorpay_payment_id,}))
+   
+  };
 
-    }
-    
-    }
-    payment()
-  },[])
-  return (
-    <div>
-      <h1>Payment </h1>
-    </div>
-  )
+  const razorpayInstance = new Razorpay({
+    ...paidPayment.data,
+    key: "rzp_test_Rk1g9fTmim96jn",
+    handler,
+  });
+  // console.log("razorpayInstance", razorpayInstance.options.amount);
+  razorpayInstance.open();
+
 }
 
-export default StudentsPayFees
+    const parseAndFormatDate = (dateString) => {
+    
+      const parsedDate = parse(dateString, "dd/MM/yyyy", new Date());
+      
+     
+      if (!isNaN(parsedDate)) {
+        return format(parsedDate, "dd/MM/yyyy");
+      }
+      return "";
+    };
+  return (
+    <div className=" p-4">
+              <section title="Payment Details">
+          <div className="flex justify-between items-center ">
+            <h2 className="text-xl">Payment Details</h2>
+            <h2 className="border bg-themeyellow text-white px-4 py-1 rounded-md cursor-pointer" onClick={handlePayment}>Fee Pay</h2>
+          </div>
+          <div className="mt-2">
+            <table
+              border="1"
+              className="p-4 text-center text-gray-600"
+              style={{ width: "100%", borderCollapse: "collapse" }}
+            >
+              <thead className="">
+                <tr className="bg-[#000380] text-white h-14">
+                  <th className="border">S.No</th>
+                  <th className="border">Batch</th>
+                  <th className="border">Fees Type</th>
+                  <th className="border">Invoice No</th>
+                  <th className="border">Paid Date</th>
+                  <th className="border">Amount</th>
+                  <th className="border">Payment Type</th>
+                  <th className="border">Payment OrderID</th>
+                  <th className="border">PaymentID</th>
+                  <th className="border">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {studentDetails.paymentRecords
+                  ? studentDetails.paymentRecords.map((value, index) => {
+                      const formattedDate = value.paid_date
+                        ? parseAndFormatDate(value.paid_date)
+                        : "";
+                      return (
+                        <>
+                          <tr className="h-10">
+                            <td className="border">{index + 1}</td>
+                            <td className="border">{studentDetails.batchID}</td>
+                            <td className="border">Monthly</td>
+                            <td className="border">Monthly</td>
+                            <td className="border">{formattedDate}</td>
+                            <td className="border">{value.received_payment ? value.received_payment : 0}</td>
+                            <td className="border">{value.paymentId ? "UPI" : "Cash"}</td>
+                            <td className="border">{value.paymentOderID ? value.paymentOderID : "cash"}</td>
+                            <td className="border">{value.paymentId ? value.paymentId : "Cash"}</td>
+                            <td className="border">{value.payment_status ? "Paid" : "unpaid"}</td>
+                          </tr>
+                        </>
+                      );
+                    })
+                  : ""}
+              </tbody>
+            </table>
+          </div>
+        </section>
+
+    </div>
+  );
+}
+
+export default StudentsPayFees;
